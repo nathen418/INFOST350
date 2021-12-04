e4e7964db14c6a0fa330a0ec893b97e4a552831a
Nate Goldsborough <nathen418@playantares.com>
2021-12-04 11:43:46 -0600

    change to txt

--- Final Project work/test.py
+++
@@ -1,7 +0,0 @@
-# validate the user typed a correct template name
-# # if not, ask the user to re-enter the template name
-# if the user typed a correct template name, then open the template
-# Print the template name, type, body, and variables
-# Ask the user to enter the values for the variables
-# Print the body with the variables filled in
-# Ask the user if they want to send the email
