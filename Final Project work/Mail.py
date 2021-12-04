99dc9cd24f37ce47d79e7ed34b929d706901f8a5
Nate Goldsborough <nathen418@playantares.com>
2021-12-04 10:39:55 -0600

    Add comments

--- Final Project work/Mail.py
+++ Final Project work/Mail.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 import json
 
 class Mail:
+    # Constructor, get data from user and env.json
     def __init__(self, to_addr, subject, contents):
         f = open('env.json')
         data = json.load(f)
@@ -36,30 +37,38 @@ class Mail:
         return self.__subject
         
     def create(self):
+        # Setup and format the email itself
         message = MIMEMultipart()
         message['From'] = self.__from
         message['To'] = self.__to
         message['Subject'] = self.__subject
         textPart = MIMEText(self.__contents, 'plain')
         message.attach(textPart)
+        # set the message object created to the class var
         self.__message = message
 
     def send(self):
+        # Create the server object
         server = smtplib.SMTP(self.__host, self.__port)
+        # Initiate a secure connection to the mail server
         server.starttls()
         try:
+            # Try to login using the secure connection and the username and application specific password
             server.login(self.__username, self.__asp)
         except:
             print('Login failed. Please try again later')
             return False
         try:
+            # Try to send the email over the previously created secure connection
             server.send_message(self.__message)
         except:
             print('Message failed to send. Please try again later')
             return False
+        # Close the connection to the server and exit
         server.quit()
     
     def validate(self):
+        # Get the user to validate that the contents of the email and associated data are correct
         print("Lets double check this is all correct")
         print("To: " + self.__to)
         print("Subject: " + self.__subject)
--- Final Project work/index.py
+++ Final Project work/index.py
@@ -3,7 +3,9 @@ import re
 import json
 import os
 
+
 def validateEmail(email):
+    # Using regex, check if the email is valid
     if len(email) > 7:
         if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
             return True
@@ -11,6 +13,7 @@ def validateEmail(email):
 
 
 def listTemplates():
+    # Get the list of templates from the templates folder and return it as a list
     templates = os.listdir('templates')
     print("These are a list of templates:")
     ret = []
@@ -20,6 +23,7 @@ def listTemplates():
     
 
 def openTemplate(templateName):
+    # try to open the template the user specified
     try:
         f = open('templates/' + templateName + '.json')
         data = json.load(f)
@@ -44,13 +48,14 @@ def main():
     subject = input("Subject: ")
 
     # List the templates and ask the user to choose one
-
     templates = listTemplates()
     print(*templates, sep = ", ")
     templateName = input("Please enter the name of the template that you want to fill out: ")
     while templateName not in templates:
         print("Template not found. Please try again.")
         templateName = input("Please enter the name of the template that you want to fill out: ")
+
+    # Open the template and get the data from the user and combine it with the template data
     data = openTemplate(templateName)
     body = str(data['template_body'])
     for var in data['template_variables']:
@@ -59,11 +64,18 @@ def main():
 
     # create the email object and send the email
     email = Mail(to_addr, subject, body)
+
+    # Create the email object
     email.create()
+    
+    # Get the user to read over the final email and make sure it is all correct
     if (not email.validate()):
+        # If the email is not valid, ask the user to re-enter the information
         main()
     else:
+        # If everything is correct, send the email
         email.send()
 
+# Run the main function
 if __name__ == "__main__":
     main()
